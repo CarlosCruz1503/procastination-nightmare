14d7fe4f5a26212d2aef3d8119a8a848b74cd078
iNickGomezB <nick.gomez.barajas@gmail.com>
2023-06-29 18:30:27 -0500

    Ultimos cambios, se extrañaran los martes de alitas

--- src/app/api/habit/habits.service.ts
+++ src/app/api/habit/habits.service.ts
@@ -12,9 +12,12 @@ export class HabitService {
   urlApi = `${environment.API_URI}/habit`;
   creatingHabit = new Habit();
   createdHabitName: BehaviorSubject<string | undefined> = new BehaviorSubject<string | undefined>(undefined);
+  allHabits: Habit [] = []
   constructor(private http: HttpClient) { 
   }
-  
+  getHabits(){
+    return this.http.get(`${this.urlApi}/getHabits`)
+  }
   createHabit(data: Habit){
     return this.http.post(`${this.urlApi}/createHabit`, data)
   }
